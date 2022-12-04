74bec6172b1522a5c12602b5e555fab14c8ed061
page0102 <31307261+page0102@users.noreply.github.com>
2022-12-04 20:36:07 +0800

    update  Myarray_test.go
    第一次作业提交

--- Myarray_test.go
+++ Myarray_test.go
@@ -1,12 +1,19 @@
 package th1
 
 /*
- 给定一个字符串数组
- [“I”,“am”,“stupid”,“and”,“weak”]
- 用 for 循环遍历该数组并修改为
- [“I”,“am”,“smart”,“and”,“strong”]
+1.  TestJump 实现：
+    给定一个字符串数组
+    [“I”,“am”,“stupid”,“and”,“weak”]
+    用 for 循环遍历该数组并修改为
+    [“I”,“am”,“smart”,“and”,“strong”]
 
-TestJump 实现
+2.  TestChanel +（producer、consummer）实现：
+    队列：
+    队列长度 10，队列元素类型为 int
+    生产者：
+    每 1 秒往队列中放入一个类型为 int 的元素，队列满时生产者可以阻塞
+    消费者：
+    每1秒从队列中获取一个元素并打印，队列为空时消费者阻塞
  
 */
 
@@ -33,4 +40,28 @@ func TestJump(t *testing.T){
 	}
 }
 
+func producer(ch chan <-int ){
 
+	for i:=0;i<10;i++{
+		ch <- i
+		time.Sleep(time.Second)
+		fmt.Println("producer insert ", i)
+	}
+	close(ch)
+}
+
+func consummer(ch chan int){
+	for i := range ch{
+		time.Sleep(time.Second)
+		fmt.Println("consummer got ", i)
+	}
+}
+
+
+func TestChanel(t *testing.T) {
+
+	ch := make(chan int,10)
+	go producer(ch)
+	consummer(ch)
+
+}
